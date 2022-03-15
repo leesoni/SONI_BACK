8559f9b77974a956fb75544b7441022c9693ab4a
lsh029 <lsh029@naver.com>
2022-03-15 15:07:30 +0900

    Initial commit

---
+++ src/main/java/com/soni/ani/AniApplication.java
@@ -0,0 +1,13 @@
+package com.soni.ani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soni/ani/AniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soni.ani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
